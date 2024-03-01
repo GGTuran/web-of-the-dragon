3b3c1e9a032e1b531c8faf926dfe6ce34e2cd977
GGTuran <tayyabturan67@gmail.com>
2024-03-01 12:01:22 +0600

    router set up with out copy paste

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC4W5BGbEL2j04o6gscEiOnczGGUAp-Cw4",
+  authDomain: "web-of-the-dragon.firebaseapp.com",
+  projectId: "web-of-the-dragon",
+  storageBucket: "web-of-the-dragon.appspot.com",
+  messagingSenderId: "849212856035",
+  appId: "1:849212856035:web:bae6f34e2f37b72c3917d6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+           <h2 className="text-3xl text-center">Home Sweet Home</h2> 
+        </div>
+    );
+};
+
+export default Home;

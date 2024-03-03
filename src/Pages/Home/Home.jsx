4f2add7390f2c710c8532684ce0d398987816464
GGTuran <tayyabturan67@gmail.com>
2024-03-03 12:40:45 +0600

    layout

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import Header from "../Shared/Header/Header";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
+import RightSideNav from "../Shared/RightsideNav/RightSideNav";
 
 
 const Home = () => {
@@ -8,6 +10,17 @@ const Home = () => {
             <Header></Header>
             <Navbar></Navbar>
            <h2 className="text-3xl text-center">Home Sweet Home</h2> 
+           <div className= " grid grd-cols-1 md:grid-cols-4 gap-7">
+            <div >
+                <LeftSideNav></LeftSideNav>
+            </div>
+            <div className="md:col-span-23">
+                <h2 className="text-4xl">Newssss</h2>
+            </div>
+            <div>
+                <RightSideNav></RightSideNav>
+            </div>
+           </div>
         </div>
     );
 };
---
+++ src/Pages/Shared/RightsideNav/RightSideNav.jsx
@@ -0,0 +1,11 @@
+
+
+const RightSideNav = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">Dan</h2>
+        </div>
+    );
+};
+
+export default RightSideNav;

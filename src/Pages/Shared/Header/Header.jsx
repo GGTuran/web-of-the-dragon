588b7b7123927f996003c149d9165cd862c9b4b9
GGTuran <tayyabturan67@gmail.com>
2024-03-03 12:54:46 +0600

    moment js used

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,9 +1,12 @@
-
+import logo from '../../../assets/logo.png'
+import moment from 'moment';
 
 const Header = () => {
     return (
         <div>
-            <h2 className="text-2xl">Homies!!!!!</h2>
+            <img src={logo} alt="" />
+            <p>Journalism without Fear or Favour</p>
+            <p className='text-xl'>{moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}</p>
         </div>
     );
 };

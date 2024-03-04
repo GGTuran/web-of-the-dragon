661b7c7a8cae4dcd027c3dc659c7c298eba24ea3
GGTuran <tayyabturan67@gmail.com>
2024-03-04 10:00:45 +0600

    using icons

--- src/Pages/Shared/RightsideNav/RightSideNav.jsx
+++ src/Pages/Shared/RightsideNav/RightSideNav.jsx
@@ -1,9 +1,15 @@
-
+import { FaGoogle } from "react-icons/fa";
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2 className="text-2xl">Dan</h2>
+            <div className="p-4 space-y-3">
+                <h2 className="text-3xl">Login With</h2>
+                <button className="btn btn-outline w-full">
+                    <FaGoogle></FaGoogle>
+                    Google</button>
+
+            </div>
         </div>
     );
 };
